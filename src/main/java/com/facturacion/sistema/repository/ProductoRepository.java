9df6283e1b26f2aa07faa973a6e152c57278aac9
luciano camavilca cardenas <lcamavilcacardenas@gmail.com>
2025-12-02 19:48:59 -0500

    Prueba de ciclo Git: Se agrega comentario de prueba en ProductoRepository

--- src/main/java/com/facturacion/sistema/repository/ProductoRepository.java
+++ src/main/java/com/facturacion/sistema/repository/ProductoRepository.java
@@ -4,6 +4,14 @@ import com.facturacion.sistema.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+// -----------------------------------------------------------------------
+// PRUEBA GIT: Repositorio modificado por Lucia Perez el 02-12-2025
+// -----------------------------------------------------------------------
+
+
+
+
 @Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
     // Métodos personalizados aquí
