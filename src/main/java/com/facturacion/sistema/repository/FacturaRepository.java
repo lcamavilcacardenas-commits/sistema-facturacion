0e9c6d2fa6f37571c29fa079d98b1ab32d55fa33
luciano camavilca cardenas <lcamavilcacardenas@gmail.com>
2025-12-03 02:41:05 -0500

    Segunda prueba Git: Agregar comentario en FacturaRepository

--- src/main/java/com/facturacion/sistema/repository/FacturaRepository.java
+++ src/main/java/com/facturacion/sistema/repository/FacturaRepository.java
@@ -4,6 +4,14 @@ import com.facturacion.sistema.model.Factura;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
+
+
+
+// -----------------------------------------------------------------------
+// SEGUNDA PRUEBA GIT: Repositorio de Factura modificado tras reinicio.
+// 
+
 @Repository
 public interface FacturaRepository extends JpaRepository<Factura, Long> {
 
